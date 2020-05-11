afe4a2e36086e530ce46f99569355252d9fc434b
banxi1988 <banxi1988@gmail.com>
2020-05-11 22:09:26 +0800

    update: 增加简单的解析请求代码,支持多资源文件返回处理,优化日志输出

--- src/website.c
+++ src/website.c
@@ -1,14 +1,39 @@
 #include <arpa/inet.h>  /* for inet_ntoa */
+#include <assert.h>     /* for assert */
 #include <fcntl.h>      /* for open O_RDONLY */
 #include <netinet/in.h> /* for IPv4 addr */
 #include <stdbool.h>
 #include <stdio.h>
-#include <string.h>     /* for strerror */
+#include <string.h>     /* for strerror strstr,strch*/
 #include <sys/errno.h>  /* for errno */
 #include <sys/mman.h>   /* for mmap */
 #include <sys/socket.h> /* for socket,bind,listen,accept etc*/
 #include <sys/stat.h>   /* for stat */
+#include <sys/time.h>   /* for gettimeofday,struct timespec */
+#include <time.h>       /* for strftime */
 #include <unistd.h>     /* for write  read*/
+
+static char *log_time() {
+  const ssize_t TIME_LEN = 64;
+  static char buf[TIME_LEN] = {0};
+  bzero(buf, TIME_LEN);
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  struct tm *tm = localtime(&tv.tv_sec);
+  int millis = tv.tv_usec / 1000;
+  size_t pos = strftime(buf, TIME_LEN, "%F %T", tm);
+  snprintf(&buf[pos], TIME_LEN - pos, ".%03d", millis);
+  return buf;
+};
+
+#define CLEAN_ERRNO() (errno == 0 ? "None" : strerror(errno))
+#define LOG_ERR(MSG, ...)                                                      \
+  fprintf(stderr, "([%s]%s:%s:%d: errno: %s) " MSG "\n", log_time(), __FILE__, \
+          __func__, __LINE__, CLEAN_ERRNO(), ##__VA_ARGS__)
+
+#define LOG_INFO(MSG, ...) \
+  fprintf(stdout, "[%s] " MSG "\n", log_time(), ##__VA_ARGS__)
+
 #define CHECK_FAIL(ret)                                       \
   ({                                                          \
     bool fail = ret == -1;                                    \
@@ -54,12 +79,39 @@ ct_socket_t create_web_server_socket(in_port_t port) {
 
 #define CRLF "\r\n"
 #define CT_BUF_SIZE 1024
-ssize_t write_http_header(ct_socket_t clientfd, size_t content_length) {
+
+const char *http_status_code_to_text(int status_code) {
+  switch (status_code) {
+    case 200:
+      return "OK";
+    case 400:
+      return "Not Found";
+    case 500:
+      return "Internal Server Error";
+    default:
+      return " ";
+  }
+}
+
+int write_http_error(ct_socket_t clientfd, int status_code) {
+  const char *msg = http_status_code_to_text(status_code);
+  const char *header_tpl =
+      "HTTP/1.1 %d %s" CRLF "Content-Type:text/html;charset=utf-8" CRLF
+      "Content-Length:0" CRLF "Connection: close" CRLF CRLF;
+  char header[CT_BUF_SIZE] = {0};
+  size_t header_len =
+      snprintf(header, CT_BUF_SIZE, header_tpl, status_code, msg);
+  return write(clientfd, header, header_len);
+}
+
+ssize_t write_http_header(ct_socket_t clientfd, const char *content_type,
+                          size_t content_length) {
   char header[CT_BUF_SIZE] = {0};
   const char *header_tpl =
-      "HTTP/1.1 200 OK" CRLF "Content-Type:text/html;charset=utf-8" CRLF
-      "Content-Length:%d" CRLF "Connection: close" CRLF CRLF;
-  size_t header_len = snprintf(header, CT_BUF_SIZE, header_tpl, content_length);
+      "HTTP/1.1 200 OK" CRLF "Content-Type:%s" CRLF "Content-Length:%d" CRLF
+      "Connection: close" CRLF CRLF;
+  size_t header_len =
+      snprintf(header, CT_BUF_SIZE, header_tpl, content_type, content_length);
   return write(clientfd, header, header_len);
 }
 
@@ -79,18 +131,101 @@ ssize_t write_http_body(ct_socket_t clientfd, const char *filename,
     return -1;
   }
   int ret = write(clientfd, map_mem, size);
-  printf("sendfile %s size: %d bytes\n", filename, ret);
   munmap(map_mem, size);
   close(fd);
   return ret;
 }
 
+long diff_timespec_to_nsec(struct timespec start, struct timespec end) {
+  if (start.tv_sec > end.tv_sec) {
+    return diff_timespec_to_nsec(end, start);
+  }
+  long s = end.tv_sec - start.tv_sec;
+  long ns = s * 1000000000L + end.tv_nsec - start.tv_nsec;
+  return ns;
+}
+
+const char *guess_content_type(const char *filename) {
+  // TODO 优化改为使用后缀判断
+  if (strstr(filename, ".png")) {
+    return "image/png";
+  } else if (strstr(filename, ".jpg")) {
+    return "image/jpg";
+  } else if (strstr(filename, ".gif")) {
+    return "image/gif";
+  } else {
+    return "text/html;charset=utf-8";
+  }
+}
+
 ssize_t send_static_file(ct_socket_t clientfd, const char *filename) {
+  struct timespec ts_start;
+  struct timespec ts_end;
+  int ret = clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts_start);
+  CT_GUARD(ret);
   size_t file_size = get_file_size(filename);
   CT_GUARD(file_size);
-  int send_cnt = write_http_header(clientfd, file_size);
+  const char *content_type = guess_content_type(filename);
+  int send_cnt = write_http_header(clientfd, content_type, file_size);
   CT_GUARD(send_cnt);
-  return write_http_body(clientfd, filename, file_size);
+  send_cnt = write_http_body(clientfd, filename, file_size);
+  ret = clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts_end);
+  CT_GUARD(ret);
+  long ns = diff_timespec_to_nsec(ts_start, ts_end);
+  double ms = ns / 1000000.0;
+  LOG_INFO("send file %s, size:%zu bytes, cost: %0.3fms", filename, file_size,
+           ms);
+  return send_cnt;
+}
+
+#define IS_FILE(mode) (S_ISREG(mode))
+ssize_t send_static_file_or_404(ct_socket_t clientfd, const char *filename) {
+  struct stat st;
+  int ret = stat(filename, &st);
+  if (CHECK_FAIL(ret)) {
+    return write_http_error(clientfd, 404);
+  }
+  if (!IS_FILE(st.st_mode)) {
+    return write_http_error(clientfd, 404);
+  }
+  return send_static_file(clientfd, filename);
+}
+
+typedef struct RequestLine {
+  char method[16];
+  char uri[256];
+  char version[16];
+} RequestLine;
+
+int parse_request_line(ct_socket_t clientfd, RequestLine *line) {
+  assert(line);
+  FILE *fin = fdopen(clientfd, "r+");
+  if (!fin) {
+    return -1;
+  }
+  char buf[CT_BUF_SIZE] = {0};
+  char *pos = fgets(buf, CT_BUF_SIZE, fin);
+  if (pos == NULL) {
+    return -1;
+  }
+  LOG_INFO("%s", buf);
+  int cnt = sscanf(buf, "%s %s %s\n", line->method, line->uri, line->version);
+  if (cnt == EOF) {
+    LOG_ERR("http request line parse error");
+    return -1;
+  }
+  return 0;
+};
+bool is_index_path(const char *uri) {
+  return strcasecmp(uri, "/") == 0 || strcasecmp(uri, "/index.html") == 0;
+}
+const char *const STATIC_ROOT = "html";
+void get_file_path_from_uri(char *file_path, const char *uri) {
+  sprintf(file_path, "%s%s", STATIC_ROOT, uri);
+  char *ptr = strchr(file_path, '?');
+  if (ptr) {
+    *ptr = '\0';
+  }
 }
 
 int main(int argc, char const *argv[]) {
@@ -108,9 +243,21 @@ int main(int argc, char const *argv[]) {
       continue;
     }
     char *client_ip = inet_ntoa(client_addr.sin_addr);
-    printf("client connected: %s:%d\n", client_ip, ntohs(client_addr.sin_port));
-    int send_cnt = send_static_file(clientfd, index_filename);
-    CHECK_FAIL(send_cnt);
+    LOG_INFO("%s:%d connected!", client_ip, ntohs(client_addr.sin_port));
+    RequestLine req;
+    int ret = parse_request_line(clientfd, &req);
+    if (CHECK_FAIL(ret)) {
+      write_http_error(clientfd, 500);
+    } else {
+      const char *filename = index_filename;
+      if (!is_index_path(req.uri)) {
+        char filename_buf[256] = {0};
+        get_file_path_from_uri(filename_buf, req.uri);
+        filename = filename_buf;
+      }
+      int send_cnt = send_static_file_or_404(clientfd, filename);
+      CHECK_FAIL(send_cnt);
+    }
     close(clientfd);
   }
 
